d34585d2336c2386be36866c37162b5e277997eb
Nicolas CARRIER <carrier.nicolas0@gmail.com>
2017-11-17 12:59:19 +0100

    use libedit instead of readline
    at least for licensing issues

--- include/console_debugger.h
+++ include/console_debugger.h
@@ -2,11 +2,17 @@
 #define CONSOLE_DEBUGGER_H
 #include <stdbool.h>
 
+#include "histedit.h"
+
 #define EMGB_CONSOLE_DEBUGGER_PROMPT "egd > "
 #define EMGB_CONSOLE_DEBUGGER_PROMPT2 "... > "
 
 struct console_debugger {
 	bool active;
+	struct editline *editline;
+	struct history *history;
+	struct HistEvent histevent;
+	int length;
 };
 
 int console_debugger_init(struct console_debugger *debugger);
--- src/console_debugger.c
+++ src/console_debugger.c
@@ -4,17 +4,16 @@
 #include <signal.h>
 #include <stdbool.h>
 
-#include <readline/readline.h>
-#include <readline/history.h>
+#include <histedit.h>
 
 #include "console_debugger.h"
 #include "log.h"
 
 struct command {
-	char *line;
+	const char *line;
 };
 
-static volatile sig_atomic_t signal_received = 0;
+static volatile sig_atomic_t signal_received = false;
 
 static void console_debugger_init_signal_handler(int signum)
 {
@@ -23,13 +22,50 @@ static void console_debugger_init_signal_handler(int signum)
 
 	printf("%s received, entering debugger\n", strsignal(signum));
 
-	signal_received = 1;
+	signal_received = true;
+}
+
+static char *console_debugger_prompt(struct editline *el)
+{
+	return EMGB_CONSOLE_DEBUGGER_PROMPT;
+}
+
+static unsigned char console_debugger_continue(struct editline *el, int ch)
+{
+	struct console_debugger *debugger;
+
+	el_get(el, EL_CLIENTDATA, &debugger);
+	debugger->active = false;
+
+	return CC_REFRESH;
 }
 
 int console_debugger_init(struct console_debugger *debugger)
 {
+	struct editline *el = debugger->editline;
+
 	memset(debugger, 0, sizeof(*debugger));
 	signal(SIGINT, console_debugger_init_signal_handler);
+	// TODO how to perform a cleanup ?
+	debugger->editline = el = el_init("emgb", stdin, stdout, stderr);
+	if (el == NULL)
+		ERR("el_init");
+	el_set(el, EL_CLIENTDATA, debugger);
+
+	/* configure history */
+	debugger->history = history_init();
+	if (debugger-> history == NULL)
+		ERR("history_init");
+	history(debugger->history, &debugger->histevent, H_SETSIZE, 100);
+	el_set(el, EL_HIST, history, debugger->history);
+
+	/* line edition setup */
+	el_set(el, EL_EDITOR, "emacs");
+	el_set(el, EL_PROMPT, console_debugger_prompt);
+
+	/* built-in functions */
+	el_set(el, EL_ADDFN, "continue", "Continue program execution",
+			console_debugger_continue);
 
 	return 0;
 }
@@ -37,9 +73,12 @@ int console_debugger_init(struct console_debugger *debugger)
 static int console_debugger_read(struct console_debugger *debugger,
 		struct command *command)
 {
-	command->line = readline(EMGB_CONSOLE_DEBUGGER_PROMPT);
+	command->line = el_gets(debugger->editline, &debugger->length);
 	if (command->line == NULL)
 		ERR("EOF received, quitting now");
+
+	history(debugger->history, &debugger->histevent, H_ENTER,
+			command->line);
 
 	return 0;
 }
@@ -47,7 +86,8 @@ static int console_debugger_read(struct console_debugger *debugger,
 static int console_debugger_execute(struct console_debugger *debugger,
 		const struct command *command)
 {
-	printf("Execute command %s\n", command->line);
+	printf("Execute command \"%.*s\"\n", strlen(command->line) - 1,
+			command->line);
 
 	return 0; // TODO stub
 }
@@ -57,8 +97,10 @@ int console_debugger_update(struct console_debugger *debugger)
 	int ret;
 	struct command command;
 
-	if (signal_received)
+	if (signal_received) {
 		debugger->active = true;
+		signal_received = false;
+	}
 
 	if (debugger->active) {
 		ret = console_debugger_read(debugger, &command);
