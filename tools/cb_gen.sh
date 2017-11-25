bb7000a15a5bf8d46ce1c9b8a4737405319f55d8
Nicolas CARRIER <carrier.nicolas0@gmail.com>
2017-11-25 17:14:17 +0100

    auto-generate cb rlc instructions code

--- tools/cb_gen.sh
+++ tools/cb_gen.sh
@@ -58,10 +58,29 @@ function generate_cb_rlc_code() {
 	local op=rlc
 	local OLDIFS=$IFS; IFS=, operands=( $1 ); IFS=$OLDIFS
 
+	echo -e "\tbool lone_bit;"
+	echo -e "\tuint8_t value;"
+	echo
 	if [ "${operands[0]}" = "(hl)" ]; then
-		:
+		echo -e "\tvalue = read8bit(s_gb->gb_register.hl, s_gb);"
 	else
-		:
+		echo -e "\tvalue = s_gb->gb_register.${operands[0]};"
+	fi
+	echo -e "\tlone_bit = BIT(7, value);"
+	echo -e "\tif (lone_bit)"
+	echo -e "\t\tFLAGS_SET(s_gb->gb_register.f, FLAGS_CARRY);"
+	echo -e "\telse"
+	echo -e "\t\tFLAGS_CLEAR(s_gb->gb_register.f, FLAGS_CARRY);"
+	echo -e "\tvalue <<= 1;"
+	echo -e "\tvalue += lone_bit;"
+	echo -e "\tif (value != 0)"
+	echo -e "\t\tFLAGS_CLEAR(s_gb->gb_register.f, FLAGS_ZERO);"
+	echo -e "\telse"
+	echo -e "\t\tFLAGS_SET(s_gb->gb_register.f, FLAGS_ZERO);"
+	if [ "${operands[0]}" = "(hl)" ]; then
+		echo -e "\twrite8bit(s_gb->gb_register.hl, value, s_gb);"
+	else
+		echo -e "\ts_gb->gb_register.${operands[0]} = value;"
 	fi
 }
 
