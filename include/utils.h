72e2b50d033dbc9d29b18497eefab963d8ce10e4
Nicolas CARRIER <carrier.nicolas0@gmail.com>
2017-11-25 15:37:10 +0100

    added some bit manipulation functions to utils

--- include/utils.h
+++ include/utils.h
@@ -6,6 +6,21 @@
 #define BIT0(v) ((v) & 1)
 #define BIT(i, v) BIT0((v) >> (i))
 
+#define FLAGS_ZERO (1 << 7)
+#define FLAGS_NEGATIVE (1 << 6)
+#define FLAGS_HALFCARRY (1 << 5)
+#define FLAGS_CARRY (1 << 4)
+
+#define FLAGS_ISZERO(f) ((f) & FLAGS_ZERO)
+#define FLAGS_ISNEGATIVE(f) ((f) & FLAGS_NEGATIVE)
+#define FLAGS_ISCARRY(f) ((f) & FLAGS_CARRY)
+#define FLAGS_ISHALFCARRY(f) ((f) & FLAGS_HALFCARRY)
+
+#define FLAGS_ISSET(f, x) ((f) & (x))
+#define FLAGS_SET(f, x) ((f) |= (x))
+#define FLAGS_CLEAR(f, x) ((f) &= ~(x))
+
+
 static inline bool str_matches(const char *s1, const char *s2)
 {
 	return strcmp(s1, s2) == 0;
