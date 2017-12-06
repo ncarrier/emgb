f92fe01e914983a112d0fc5a11ec37e2ef4f408d
Nicolas Carrier <carrier.nicolas0@gmail.com>
2017-12-06 07:32:29 +0100

    rewritten all cb instructions with here docs

--- tools/cb_gen.sh
+++ tools/cb_gen.sh
@@ -1,273 +1,325 @@
+regs="s_gb->gb_register"
+
 function generate_cb_bit_code() {
-	local op=bit
-	local OLDIFS=$IFS; IFS=, operands=( $1 ); IFS=$OLDIFS
+	local operands
+	local value
+
+	OLDIFS=$IFS; IFS=, operands=( $1 ); IFS=$OLDIFS
 
 	if [ "${operands[1]}" = "(hl)" ]; then
-		echo -e "\tif (BIT(${operands[0]}, read8bit(s_gb->gb_register.hl, s_gb)))"
+		value="read8bit(${regs}.hl, s_gb)"
 	else
-		echo -e "\tif (BIT(${operands[0]}, s_gb->gb_register.${operands[1]}))"
+		value="${regs}.${operands[1]}"
 	fi
-	echo -e "\t\tFLAGS_CLEAR(s_gb->gb_register.f, FLAGS_ZERO);"
-	echo -e "\telse"
-	echo -e "\t\tFLAGS_SET(s_gb->gb_register.f, FLAGS_ZERO);"
+
+	cat <<here_doc_delim
+	if (BIT(${operands[0]}, ${value}))
+		FLAGS_CLEAR(${regs}.f, FLAGS_ZERO);
+	else
+		FLAGS_SET(${regs}.f, FLAGS_ZERO);
+here_doc_delim
 }
 
 function generate_cb_res_code() {
-	local op=res
-	local OLDIFS=$IFS; IFS=, operands=( $1 ); IFS=$OLDIFS
+	local operands
+	OLDIFS=$IFS; IFS=, operands=( $1 ); IFS=$OLDIFS
 	local mask="~(1 << ${operands[0]})"
 
 	if [ "${operands[1]}" = "(hl)" ]; then
-		echo -e "\twrite8bit(s_gb->gb_register.hl, read8bit(s_gb->gb_register.hl, s_gb) & ${mask}, s_gb);"
+		echo -e "\twrite8bit(${regs}.hl, read8bit(${regs}.hl, s_gb) & ${mask}, s_gb);"
 	else
-		echo -e "\ts_gb->gb_register.${operands[1]} &= ${mask};"
+		echo -e "\t${regs}.${operands[1]} &= ${mask};"
 	fi
 }
 
 function generate_cb_rl_code() {
-	local op=rl
-	local OLDIFS=$IFS; IFS=, operands=( $1 ); IFS=$OLDIFS
+	local operands
 
-	echo -e "\tbool carry;"
-	echo -e "\tuint8_t value;"
-	echo
-	echo -e "\tcarry = FLAGS_ISSET(s_gb->gb_register.f, FLAGS_CARRY);"
+	OLDIFS=$IFS; IFS=, operands=( $1 ); IFS=$OLDIFS
+
 	if [ "${operands[0]}" = "(hl)" ]; then
-		echo -e "\tvalue = read8bit(s_gb->gb_register.hl, s_gb);"
+		value="read8bit(${regs}.hl, s_gb)"
 	else
-		echo -e "\tvalue = s_gb->gb_register.${operands[0]};"
+		value="${regs}.${operands[0]}"
 	fi
-	echo -e "\tif (BIT(7, value) != 0)"
-	echo -e "\t\tFLAGS_SET(s_gb->gb_register.f, FLAGS_CARRY);"
-	echo -e "\telse"
-	echo -e "\t\tFLAGS_CLEAR(s_gb->gb_register.f, FLAGS_CARRY);"
-	echo -e "\tvalue <<= 1;"
-	echo -e "\tvalue += carry;"
-	echo -e "\tif (value != 0)"
-	echo -e "\t\tFLAGS_CLEAR(s_gb->gb_register.f, FLAGS_ZERO);"
-	echo -e "\telse"
-	echo -e "\t\tFLAGS_SET(s_gb->gb_register.f, FLAGS_ZERO);"
-	if [ "${operands[0]}" = "(hl)" ]; then
-		echo -e "\twrite8bit(s_gb->gb_register.hl, value, s_gb);"
+
+	cat <<here_doc_delim
+	bool carry;
+	uint8_t value;
+
+	carry = FLAGS_ISSET(${regs}.f, FLAGS_CARRY);
+	value = ${value};
+	if (BIT(7, value) != 0)
+		FLAGS_SET(${regs}.f, FLAGS_CARRY);
 	else
-		echo -e "\ts_gb->gb_register.${operands[0]} = value;"
+		FLAGS_CLEAR(${regs}.f, FLAGS_CARRY);
+	value <<= 1;
+	value += carry;
+	if (value != 0)
+		FLAGS_CLEAR(${regs}.f, FLAGS_ZERO);
+	else
+		FLAGS_SET(${regs}.f, FLAGS_ZERO);
+here_doc_delim
+
+	if [ "${operands[0]}" = "(hl)" ]; then
+		echo -e "\twrite8bit(${regs}.hl, value, s_gb);"
+	else
+		echo -e "\t${regs}.${operands[0]} = value;"
 	fi
 }
 
 function generate_cb_rlc_code() {
-	local op=rlc
-	local OLDIFS=$IFS; IFS=, operands=( $1 ); IFS=$OLDIFS
+	local operands
 
-	echo -e "\tbool lone_bit;"
-	echo -e "\tuint8_t value;"
-	echo
+	OLDIFS=$IFS; IFS=, operands=( $1 ); IFS=$OLDIFS
+
 	if [ "${operands[0]}" = "(hl)" ]; then
-		echo -e "\tvalue = read8bit(s_gb->gb_register.hl, s_gb);"
+		value="read8bit(${regs}.hl, s_gb)"
 	else
-		echo -e "\tvalue = s_gb->gb_register.${operands[0]};"
+		value="${regs}.${operands[0]}"
 	fi
-	echo -e "\tlone_bit = BIT(7, value);"
-	echo -e "\tif (lone_bit)"
-	echo -e "\t\tFLAGS_SET(s_gb->gb_register.f, FLAGS_CARRY);"
-	echo -e "\telse"
-	echo -e "\t\tFLAGS_CLEAR(s_gb->gb_register.f, FLAGS_CARRY);"
-	echo -e "\tvalue <<= 1;"
-	echo -e "\tvalue += lone_bit;"
-	echo -e "\tif (value != 0)"
-	echo -e "\t\tFLAGS_CLEAR(s_gb->gb_register.f, FLAGS_ZERO);"
-	echo -e "\telse"
-	echo -e "\t\tFLAGS_SET(s_gb->gb_register.f, FLAGS_ZERO);"
-	if [ "${operands[0]}" = "(hl)" ]; then
-		echo -e "\twrite8bit(s_gb->gb_register.hl, value, s_gb);"
+
+	cat <<here_doc_delim
+	bool lone_bit;
+	uint8_t value;
+
+	value = ${value};
+	lone_bit = BIT(7, value);
+	if (lone_bit)
+		FLAGS_SET(${regs}.f, FLAGS_CARRY);
 	else
-		echo -e "\ts_gb->gb_register.${operands[0]} = value;"
+		FLAGS_CLEAR(${regs}.f, FLAGS_CARRY);
+	value <<= 1;
+	value += lone_bit;
+	if (value != 0)
+		FLAGS_CLEAR(${regs}.f, FLAGS_ZERO);
+	else
+		FLAGS_SET(${regs}.f, FLAGS_ZERO);
+here_doc_delim
+
+	if [ "${operands[0]}" = "(hl)" ]; then
+		echo -e "\twrite8bit(${regs}.hl, value, s_gb);"
+	else
+		echo -e "\t${regs}.${operands[0]} = value;"
 	fi
 }
 
 function generate_cb_rr_code() {
-	local op=rr
-	local OLDIFS=$IFS; IFS=, operands=( $1 ); IFS=$OLDIFS
+	local operands
 
-	echo -e "\tbool carry;"
-	echo -e "\tuint8_t value;"
-	echo
-	echo -e "\tcarry = FLAGS_ISSET(s_gb->gb_register.f, FLAGS_CARRY);"
+	OLDIFS=$IFS; IFS=, operands=( $1 ); IFS=$OLDIFS
+
 	if [ "${operands[0]}" = "(hl)" ]; then
-		echo -e "\tvalue = read8bit(s_gb->gb_register.hl, s_gb);"
+		value="read8bit(${regs}.hl, s_gb)"
 	else
-		echo -e "\tvalue = s_gb->gb_register.${operands[0]};"
+		value="${regs}.${operands[0]}"
 	fi
-	echo -e "\tif (BIT(0, value) != 0)"
-	echo -e "\t\tFLAGS_SET(s_gb->gb_register.f, FLAGS_CARRY);"
-	echo -e "\telse"
-	echo -e "\t\tFLAGS_CLEAR(s_gb->gb_register.f, FLAGS_CARRY);"
-	echo -e "\tvalue >>= 1;"
-	echo -e "\tvalue += (carry << 7);"
-	echo -e "\tif (value != 0)"
-	echo -e "\t\tFLAGS_CLEAR(s_gb->gb_register.f, FLAGS_ZERO);"
-	echo -e "\telse"
-	echo -e "\t\tFLAGS_SET(s_gb->gb_register.f, FLAGS_ZERO);"
-	if [ "${operands[0]}" = "(hl)" ]; then
-		echo -e "\twrite8bit(s_gb->gb_register.hl, value, s_gb);"
+
+	cat <<here_doc_delim
+	bool carry;
+	uint8_t value;
+
+	carry = FLAGS_ISSET(${regs}.f, FLAGS_CARRY);
+	value = ${value};
+	if (BIT(0, value) != 0)
+		FLAGS_SET(${regs}.f, FLAGS_CARRY);
 	else
-		echo -e "\ts_gb->gb_register.${operands[0]} = value;"
+		FLAGS_CLEAR(${regs}.f, FLAGS_CARRY);
+	value >>= 1;
+	value += (carry << 7);
+	if (value != 0)
+		FLAGS_CLEAR(${regs}.f, FLAGS_ZERO);
+	else
+		FLAGS_SET(${regs}.f, FLAGS_ZERO);
+here_doc_delim
+
+	if [ "${operands[0]}" = "(hl)" ]; then
+		echo -e "\twrite8bit(${regs}.hl, value, s_gb);"
+	else
+		echo -e "\t${regs}.${operands[0]} = value;"
 	fi
 }
 
 function generate_cb_rrc_code() {
-	local op=rrc
-	local OLDIFS=$IFS; IFS=, operands=( $1 ); IFS=$OLDIFS
+	local operands
 
-	echo -e "\tbool lone_bit;"
-	echo -e "\tuint8_t value;"
-	echo
+	OLDIFS=$IFS; IFS=, operands=( $1 ); IFS=$OLDIFS
+
 	if [ "${operands[0]}" = "(hl)" ]; then
-		echo -e "\tvalue = read8bit(s_gb->gb_register.hl, s_gb);"
+		value="read8bit(${regs}.hl, s_gb)"
 	else
-		echo -e "\tvalue = s_gb->gb_register.${operands[0]};"
+		value="${regs}.${operands[0]}"
 	fi
-	echo -e "\tlone_bit = BIT(0, value);"
-	echo -e "\tif (lone_bit)"
-	echo -e "\t\tFLAGS_SET(s_gb->gb_register.f, FLAGS_CARRY);"
-	echo -e "\telse"
-	echo -e "\t\tFLAGS_CLEAR(s_gb->gb_register.f, FLAGS_CARRY);"
-	echo -e "\tvalue >>= 1;"
-	echo -e "\tvalue += (lone_bit << 7);"
-	echo -e "\tif (value != 0)"
-	echo -e "\t\tFLAGS_CLEAR(s_gb->gb_register.f, FLAGS_ZERO);"
-	echo -e "\telse"
-	echo -e "\t\tFLAGS_SET(s_gb->gb_register.f, FLAGS_ZERO);"
-	if [ "${operands[0]}" = "(hl)" ]; then
-		echo -e "\twrite8bit(s_gb->gb_register.hl, value, s_gb);"
+
+	cat <<here_doc_delim
+	bool lone_bit;
+	uint8_t value;
+
+	value = ${value};
+	lone_bit = BIT(0, value);
+	if (lone_bit)
+		FLAGS_SET(${regs}.f, FLAGS_CARRY);
 	else
-		echo -e "\ts_gb->gb_register.${operands[0]} = value;"
+		FLAGS_CLEAR(${regs}.f, FLAGS_CARRY);
+	value >>= 1;
+	value += (lone_bit << 7);
+	if (value != 0)
+		FLAGS_CLEAR(${regs}.f, FLAGS_ZERO);
+	else
+		FLAGS_SET(${regs}.f, FLAGS_ZERO);
+here_doc_delim
+
+	if [ "${operands[0]}" = "(hl)" ]; then
+		echo -e "\twrite8bit(${regs}.hl, value, s_gb);"
+	else
+		echo -e "\t${regs}.${operands[0]} = value;"
 	fi
 }
 
 function generate_cb_set_code() {
-	local op=set
 	local OLDIFS=$IFS; IFS=, operands=( $1 ); IFS=$OLDIFS
 	local mask="(1 << ${operands[0]})"
 
 	if [ "${operands[1]}" = "(hl)" ]; then
-		echo -e "\twrite8bit(s_gb->gb_register.hl, read8bit(s_gb->gb_register.hl, s_gb) | ${mask}, s_gb);"
+		echo -e "\twrite8bit(${regs}.hl, read8bit(${regs}.hl, s_gb) | ${mask}, s_gb);"
 	else
-		echo -e "\ts_gb->gb_register.${operands[1]} |= ${mask};"
+		echo -e "\t${regs}.${operands[1]} |= ${mask};"
 	fi
 }
 
 function generate_cb_sla_code() {
-	local op=sla
-	local OLDIFS=$IFS; IFS=, operands=( $1 ); IFS=$OLDIFS
+	local operands
 
-	echo -e "\tuint8_t value;"
-	echo
+	OLDIFS=$IFS; IFS=, operands=( $1 ); IFS=$OLDIFS
+
 	if [ "${operands[0]}" = "(hl)" ]; then
-		echo -e "\tvalue = read8bit(s_gb->gb_register.hl, s_gb);"
+		value="read8bit(${regs}.hl, s_gb)"
 	else
-		echo -e "\tvalue = s_gb->gb_register.${operands[0]};"
+		value="${regs}.${operands[0]}"
 	fi
-	echo -e "\tif (BIT(7, value) != 0)"
-	echo -e "\t\tFLAGS_SET(s_gb->gb_register.f, FLAGS_CARRY);"
-	echo -e "\telse"
-	echo -e "\t\tFLAGS_CLEAR(s_gb->gb_register.f, FLAGS_CARRY);"
-	echo -e "\tvalue <<= 1;"
-	echo -e "\tif (value != 0)"
-	echo -e "\t\tFLAGS_CLEAR(s_gb->gb_register.f, FLAGS_ZERO);"
-	echo -e "\telse"
-	echo -e "\t\tFLAGS_SET(s_gb->gb_register.f, FLAGS_ZERO);"
-	if [ "${operands[0]}" = "(hl)" ]; then
-		echo -e "\twrite8bit(s_gb->gb_register.hl, value, s_gb);"
+
+	cat <<here_doc_delim
+	uint8_t value;
+
+	value = ${value};
+	if (BIT(7, value) != 0)
+		FLAGS_SET(${regs}.f, FLAGS_CARRY);
 	else
-		echo -e "\ts_gb->gb_register.${operands[0]} = value;"
+		FLAGS_CLEAR(${regs}.f, FLAGS_CARRY);
+	value <<= 1;
+	if (value != 0)
+		FLAGS_CLEAR(${regs}.f, FLAGS_ZERO);
+	else
+		FLAGS_SET(${regs}.f, FLAGS_ZERO);
+here_doc_delim
+
+	if [ "${operands[0]}" = "(hl)" ]; then
+		echo -e "\twrite8bit(${regs}.hl, value, s_gb);"
+	else
+		echo -e "\t${regs}.${operands[0]} = value;"
 	fi
 }
 
 function generate_cb_swap_code() {
-	local op=swap
-	local OLDIFS=$IFS; IFS=, operands=( $1 ); IFS=$OLDIFS
+	local operands
 
-	echo -e "\t/* sll opcodes are replaced with nibble swapping for gb */"
-	echo -e "\tuint8_t value;"
-	echo
+	OLDIFS=$IFS; IFS=, operands=( $1 ); IFS=$OLDIFS
+
 	if [ "${operands[0]}" = "(hl)" ]; then
-		echo -e "\tvalue = read8bit(s_gb->gb_register.hl, s_gb);"
+		value="read8bit(${regs}.hl, s_gb)"
 	else
-		echo -e "\tvalue = s_gb->gb_register.${operands[0]};"
+		value="${regs}.${operands[0]}"
 	fi
-	echo -e "\tvalue = ((value & 0xf) << 4) | ((value & 0xf0) >> 4);"
-	echo -e "\tif (value != 0)"
-	echo -e "\t\tFLAGS_CLEAR(s_gb->gb_register.f, FLAGS_ZERO);"
-	echo -e "\telse"
-	echo -e "\t\tFLAGS_SET(s_gb->gb_register.f, FLAGS_ZERO);"
-	if [ "${operands[0]}" = "(hl)" ]; then
-		echo -e "\twrite8bit(s_gb->gb_register.hl, value, s_gb);"
+
+	cat <<here_doc_delim
+	/* sll opcodes are replaced with nibble swapping for gb */
+	uint8_t value;
+
+	value = ${value};
+	value = ((value & 0xf) << 4) | ((value & 0xf0) >> 4);
+	if (value != 0)
+		FLAGS_CLEAR(${regs}.f, FLAGS_ZERO);
 	else
-		echo -e "\ts_gb->gb_register.${operands[0]} = value;"
+		FLAGS_SET(${regs}.f, FLAGS_ZERO);
+here_doc_delim
+
+	if [ "${operands[0]}" = "(hl)" ]; then
+		echo -e "\twrite8bit(${regs}.hl, value, s_gb);"
+	else
+		echo -e "\t${regs}.${operands[0]} = value;"
 	fi
 	echo -e "\tCLEAR_CARRY();"
 }
 
 function generate_cb_sra_code() {
-	local op=sra
-	local OLDIFS=$IFS; IFS=, operands=( $1 ); IFS=$OLDIFS
+	local operands
 
-	echo -e "\tuint8_t value;"
-	echo -e "\tbool bit7;"
-	echo
+	OLDIFS=$IFS; IFS=, operands=( $1 ); IFS=$OLDIFS
+
 	if [ "${operands[0]}" = "(hl)" ]; then
-		echo -e "\tvalue = read8bit(s_gb->gb_register.hl, s_gb);"
+		value="read8bit(${regs}.hl, s_gb)"
 	else
-		echo -e "\tvalue = s_gb->gb_register.${operands[0]};"
+		value="${regs}.${operands[0]}"
 	fi
-	echo -e "\tbit7 = BIT(7, value);"
-	echo -e "\tif (BIT(0, value) != 0)"
-	echo -e "\t\tFLAGS_SET(s_gb->gb_register.f, FLAGS_CARRY);"
-	echo -e "\telse"
-	echo -e "\t\tFLAGS_CLEAR(s_gb->gb_register.f, FLAGS_CARRY);"
-	echo -e "\tvalue >>= 1;"
-	echo -e "\tif (value != 0)"
-	echo -e "\t\tFLAGS_CLEAR(s_gb->gb_register.f, FLAGS_ZERO);"
-	echo -e "\telse"
-	echo -e "\t\tFLAGS_SET(s_gb->gb_register.f, FLAGS_ZERO);"
 
-	echo -e "\tif (bit7)"
-	echo -e "\t\tFLAGS_SET(value, (1 << 7));"
-	echo -e "\telse"
-	echo -e "\t\tFLAGS_CLEAR(value, (1 << 7));"
+	cat <<here_doc_delim
+	uint8_t value;
+	bool bit7;
+
+	value = ${value};
+	bit7 = BIT(7, value);
+	if (BIT(0, value) != 0)
+		FLAGS_SET(${regs}.f, FLAGS_CARRY);
+	else
+		FLAGS_CLEAR(${regs}.f, FLAGS_CARRY);
+	value >>= 1;
+	if (value != 0)
+		FLAGS_CLEAR(${regs}.f, FLAGS_ZERO);
+	else
+		FLAGS_SET(${regs}.f, FLAGS_ZERO);
+	if (bit7)
+		FLAGS_SET(value, (1 << 7));
+	else
+		FLAGS_CLEAR(value, (1 << 7));
+here_doc_delim
 
 	if [ "${operands[0]}" = "(hl)" ]; then
-		echo -e "\twrite8bit(s_gb->gb_register.hl, value, s_gb);"
+		echo -e "\twrite8bit(${regs}.hl, value, s_gb);"
 	else
-		echo -e "\ts_gb->gb_register.${operands[0]} = value;"
+		echo -e "\t${regs}.${operands[0]} = value;"
 	fi
 }
 
 function generate_cb_srl_code() {
-	local op=srl
-	local OLDIFS=$IFS; IFS=, operands=( $1 ); IFS=$OLDIFS
+	local operands
 
-	echo -e "\tuint8_t value;"
-	echo
+	OLDIFS=$IFS; IFS=, operands=( $1 ); IFS=$OLDIFS
+
 	if [ "${operands[0]}" = "(hl)" ]; then
-		echo -e "\tvalue = read8bit(s_gb->gb_register.hl, s_gb);"
+		value="read8bit(${regs}.hl, s_gb)"
 	else
-		echo -e "\tvalue = s_gb->gb_register.${operands[0]};"
+		value="${regs}.${operands[0]}"
 	fi
-	echo -e "\tif (BIT(0, value) != 0)"
-	echo -e "\t\tFLAGS_SET(s_gb->gb_register.f, FLAGS_CARRY);"
-	echo -e "\telse"
-	echo -e "\t\tFLAGS_CLEAR(s_gb->gb_register.f, FLAGS_CARRY);"
-	echo -e "\tvalue >>= 1;"
-	echo -e "\tFLAGS_CLEAR(value, 1 << 7);"
-	echo -e "\tif (value != 0)"
-	echo -e "\t\tFLAGS_CLEAR(s_gb->gb_register.f, FLAGS_ZERO);"
-	echo -e "\telse"
-	echo -e "\t\tFLAGS_SET(s_gb->gb_register.f, FLAGS_ZERO);"
-	if [ "${operands[0]}" = "(hl)" ]; then
-		echo -e "\twrite8bit(s_gb->gb_register.hl, value, s_gb);"
+
+	cat <<here_doc_delim
+	uint8_t value;
+
+	value = ${value};
+	if (BIT(0, value) != 0)
+		FLAGS_SET(${regs}.f, FLAGS_CARRY);
 	else
-		echo -e "\ts_gb->gb_register.${operands[0]} = value;"
+		FLAGS_CLEAR(${regs}.f, FLAGS_CARRY);
+	value >>= 1;
+	FLAGS_CLEAR(value, 1 << 7);
+	if (value != 0)
+		FLAGS_CLEAR(${regs}.f, FLAGS_ZERO);
+	else
+		FLAGS_SET(${regs}.f, FLAGS_ZERO);
+here_doc_delim
+
+	if [ "${operands[0]}" = "(hl)" ]; then
+		echo -e "\twrite8bit(${regs}.hl, value, s_gb);"
+	else
+		echo -e "\t${regs}.${operands[0]} = value;"
 	fi
 }
-
