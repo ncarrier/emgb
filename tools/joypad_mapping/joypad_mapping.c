64df09de9a77ccbc74fbf529fb1baed33b3f9cb5
Nicolas Carrier <carrier.nicolas0@gmail.com>
2017-12-28 20:37:26 +0100

    warningfix: use "%s" instead of passing directly the string

--- tools/joypad_mapping/joypad_mapping.c
+++ tools/joypad_mapping/joypad_mapping.c
@@ -361,7 +361,7 @@ const char cnorm[] = { 0x1b, 0x5b, 0x3f, 0x31, 0x32, 0x6c, 0x1b, 0x5b, 0x3f,
 
 static void restore_cursor(void)
 {
-	printf(cnorm);
+	printf("%s", cnorm);
 }
 
 static bool handle_test_event(const char *mappings_dir,
@@ -494,7 +494,7 @@ static int test_joypad(const char *mappings_dir)
 
 	reset_joystick_config(&joystick_config);
 
-	printf(civis);
+	printf("%s", civis);
 	atexit(restore_cursor);
 	printf("Waiting for joystick detection\n");
 	ret = SDL_Init(SDL_INIT_JOYSTICK);
@@ -599,7 +599,7 @@ int main(int argc, char **argv)
 	printf("\tHats: %d\n", joystick_config.joystick.num.hats);
 	printf("\tButtons: %d\n", joystick_config.joystick.num.buttons);
 
-	printf(civis);
+	printf("%s", civis);
 	atexit(restore_cursor);
 	button = GB_BUTTON_INVALID;
 	next_button(&button);
