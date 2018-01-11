49d9c2cb02c17ed2d0b9da818a57f89c2aea631d
Nicolas Carrier <carrier.nicolas0@gmail.com>
2018-01-11 07:39:42 +0100

    added a style checker script

---
+++ tools/checkstyle.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -eu
+
+me=$(realpath $0)
+my_dir=$(dirname ${me})/
+
+checkpatch=${my_dir}checkpatch2.pl
+
+sources=$(find -name '*.c' -o -name '*.h')
+
+exclude=""
+
+for f in ${sources}; do
+	echo '**** '$f' ****'
+	skip="false"
+	for e in ${exclude}; do
+		if [ "${f}" = "${e}" ]; then
+			echo " -> skipping ${f}"
+			skip="true"
+			break
+		fi
+	done
+	if [ "${skip}" = "true" ]; then
+		continue
+	fi
+	${checkpatch} --no-tree -f ${f}
+	echo -e '\n'
+done
