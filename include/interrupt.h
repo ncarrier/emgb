bad0c2aab7216066bee89473a3ebe87cd7e8e54d
Nicolas Carrier <carrier.nicolas0@gmail.com>
2018-01-15 07:07:58 +0100

    code style of interrupt.{c,h}

--- include/interrupt.h
+++ include/interrupt.h
@@ -1,14 +1,13 @@
 #ifndef INTERRUPT
 #define INTERRUPT
 
-
 struct s_gb;
-void doInterupt(struct s_gb * gb_s);
-void timer(struct s_gb * gb_s);
-void serial(struct s_gb * gb_s);
-void joypad(struct s_gb * gb_s);
-void lcd(struct s_gb * gb_s);
-void vblanck(struct s_gb * gb_s);
+void doInterupt(struct s_gb *gb_s);
+void timer(struct s_gb *gb_s);
+void serial(struct s_gb *gb_s);
+void joypad(struct s_gb *gb_s);
+void lcd(struct s_gb *gb_s);
+void vblank(struct s_gb *gb_s);
 
 #define INT_VBLANK (1 << 0)
 #define INT_LCDSTAT (1 << 1)
@@ -16,12 +15,10 @@ void vblanck(struct s_gb * gb_s);
 #define INT_SERIAL (1 << 3)
 #define INT_JOYPAD (1 << 4)
 
-
-struct				s_interupt
-{
-	unsigned char	interMaster;
-	unsigned char	interFlag;
-	unsigned char	interEnable;
+struct s_interupt {
+	unsigned char interMaster;
+	unsigned char interFlag;
+	unsigned char interEnable;
 };
 
 #endif
--- src/interrupt.c
+++ src/interrupt.c
@@ -2,9 +2,8 @@
 
 #include "GB.h"
 
-void vblanck(struct s_gb * gb_s)
+void vblank(struct s_gb *gb_s)
 {
-	gb_s->gb_cpu.totalTick += 12;
 	gb_s->gb_interrupts.interMaster = 0;
 	gb_s->gb_register.sp -= 2;
 	write16bitToAddr(gb_s->gb_register.sp, gb_s->gb_register.pc, gb_s);
@@ -12,9 +11,8 @@ void vblanck(struct s_gb * gb_s)
 
 }
 
-void lcd(struct s_gb * gb_s)
+void lcd(struct s_gb *gb_s)
 {
-	gb_s->gb_cpu.totalTick += 12;
 	gb_s->gb_interrupts.interMaster = 0;
 	printf("doing lcdc !\n");
 	gb_s->gb_register.sp -= 2;
@@ -22,42 +20,35 @@ void lcd(struct s_gb * gb_s)
 	gb_s->gb_register.pc = 0x48;
 }
 
-void joypad(struct s_gb * gb_s)
+void joypad(struct s_gb *gb_s)
 {
 	gb_s->gb_interrupts.interMaster = 0;
-	gb_s->gb_cpu.totalTick += 12;
 	gb_s->gb_register.sp -= 2;
 	write16bitToAddr(gb_s->gb_register.sp, gb_s->gb_register.pc, gb_s);
 	gb_s->gb_register.pc = 0x60;
 }
 
-void serial(struct s_gb * gb_s)
+void serial(struct s_gb *gb_s)
 {
 	gb_s->gb_interrupts.interMaster = 0;
-	gb_s->gb_cpu.totalTick += 12;
 	gb_s->gb_register.sp -= 2;
 	write16bitToAddr(gb_s->gb_register.sp, gb_s->gb_register.pc, gb_s);
 	gb_s->gb_register.pc = 0x58;
 }
 
-void timer(struct s_gb * gb_s)
+void timer(struct s_gb *gb_s)
 {
 	debug(gb_s);
 	gb_s->gb_interrupts.interMaster = 0;
-	gb_s->gb_cpu.totalTick += 12;
 	gb_s->gb_register.sp -= 2;
 	write16bitToAddr(gb_s->gb_register.sp, gb_s->gb_register.pc, gb_s);
 	gb_s->gb_register.pc = 0x50;
 }
 
-void doInterupt(struct s_gb * gb_s)
+void doInterupt(struct s_gb *gb_s)
 {
 	unsigned char inter;
 
-//	printf("master, %"PRIu8"master, %"PRIu8", %"PRIu8"\n",
-//			gb_s->gb_interrupts.interMaster,
-//			gb_s->gb_interrupts.interEnable,
-//			gb_s->gb_interrupts.interFlag);
 	if (gb_s->gb_interrupts.interFlag & INT_JOYPAD)
 		gb_s->gb_cpu.stopped = false;
 	gb_s->gb_cpu.halted = false;
@@ -65,15 +56,11 @@ void doInterupt(struct s_gb * gb_s)
 			&& gb_s->gb_interrupts.interFlag) {
 		inter = gb_s->gb_interrupts.interEnable
 				& gb_s->gb_interrupts.interFlag;
-//		if (gb_s->gb_interrupts.interFlag != 0)
-//		gb_s->gb_cpu.halted = false;
-		if (inter != 0) {
+		if (inter != 0)
 			gb_s->gb_cpu.halted = false;
-//			puts("de-halted");
-		}
 		if (inter & INT_VBLANK) {
 			gb_s->gb_interrupts.interFlag &= ~(INT_VBLANK);
-			vblanck(gb_s);
+			vblank(gb_s);
 		}
 		if (inter & INT_LCDSTAT) {
 			printf("LCD interrupt\n");
@@ -81,8 +68,8 @@ void doInterupt(struct s_gb * gb_s)
 			lcd(gb_s);
 		}
 		if (inter & INT_TIMER) {
-//			printf("TIMER interrupt\n");
 			timer(gb_s);
+/*			printf("TIMER interrupt\n"); */
 			gb_s->gb_interrupts.interFlag &= ~(INT_TIMER);
 		}
 		if (inter & INT_JOYPAD) {
