f411065b68bc6c4a04899162c081a1aadea6eada
Nicolas Carrier <carrier.nicolas0@gmail.com>
2018-01-28 06:35:49 +0100

    document why using a memcpy isn't possible, for DMA transfer

--- src/io.c
+++ src/io.c
@@ -5,6 +5,10 @@ static void oam_transfert(struct memory *memory)
 	int pos;
 	unsigned short oamsrc;
 
+	/*
+	 * In theory, this could be implemented with a memcpy, but because of
+	 * banking, it's not possible.
+	 */
 	oamsrc = memory->spec_reg.dma << 8;
 	for (pos = 0; pos < 0xa0; pos++)
 		memory->oam[pos] = read8bit(memory, oamsrc + pos);
