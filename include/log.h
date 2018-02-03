07cc39451f552d3e936b5a04be7aa614e90e8f9a
Nicolas Carrier <carrier.nicolas0@gmail.com>
2018-02-03 13:56:27 +0100

    make DBG variadic

--- include/log.h
+++ include/log.h
@@ -7,7 +7,9 @@
 		__FILE__, __LINE__); \
 		exit(0); \
 } while (0)
-#define DBG(s) printf("DBG:: %s ---- %s line %d\n", s, __FILE__, __LINE__)
+#define DBG(s, ...) do {fprintf(stderr, "DBG:: " s " ---- %s line %d\n", ##__VA_ARGS__, \
+		__FILE__, __LINE__); \
+} while (0)
 #define SUCC(s) printf("OK:: %s\n", s)
 
 #endif /* INCLUDE_LOG_H_ */
