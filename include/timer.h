64416fe090f30214c6a7caabd93102416ed0abdc
Nicolas Carrier <carrier.nicolas0@gmail.com>
2018-02-03 06:18:49 +0100

    direct spec_reg access in timer, instead of write8bit

--- include/timer.h
+++ include/timer.h
@@ -8,15 +8,15 @@
 #define TAC_INPUT_CLOCK_SELECT(tac) ((tac) & 0x03)
 #define TAC_TIMER_ENABLED(tac) ((tac) & 0x04)
 
-struct memory;
+struct spec_reg;
 struct timer {
-	struct memory *memory;
+	struct spec_reg *spec_reg;
 
 	/* serialized fields */
 	uint32_t timer_count;
 };
 
-void timer_init(struct timer *timer, struct memory *memory);
+void timer_init(struct timer *timer, struct spec_reg *spec_reg);
 void timer_update(struct timer *timer, unsigned cycles);
 int timer_save(const struct timer *timer, FILE *f);
 int timer_restore(struct timer *timer, FILE *f);
--- src/timer.c
+++ src/timer.c
@@ -14,35 +14,32 @@ static const uint32_t periods_table[] = {
 		[3] = 256u,
 };
 
-void timer_init(struct timer *timer, struct memory *memory)
+void timer_init(struct timer *timer, struct spec_reg *spec_reg)
 {
-	timer->memory = memory;
+	timer->spec_reg = spec_reg;
 	timer->timer_count = 0;
 }
 
 void timer_update(struct timer *timer, unsigned cycles)
 {
-	struct memory *memory;
 	uint8_t input_clock_sel;
 	uint32_t period;
 
-	memory = timer->memory;
-	if (!TAC_TIMER_ENABLED(memory->spec_reg.tac))
+	if (!TAC_TIMER_ENABLED(timer->spec_reg->tac))
 		return;
 
 	timer->timer_count += cycles;
-	input_clock_sel = TAC_INPUT_CLOCK_SELECT(timer->memory->spec_reg.tac);
+	input_clock_sel = TAC_INPUT_CLOCK_SELECT(timer->spec_reg->tac);
 	period = periods_table[input_clock_sel];
 	if (timer->timer_count < period)
 		return;
 
 	timer->timer_count -= period;
-	if (memory->spec_reg.tima == 0xffu) {
-		write8bit(memory, SPECIAL_REGISTER_TIMA, memory->spec_reg.tma);
-		memory->spec_reg.ifl |= INT_TIMER;
+	if (timer->spec_reg->tima == 0xffu) {
+		timer->spec_reg->tima = timer->spec_reg->tma;
+		timer->spec_reg->ifl |= INT_TIMER;
 	} else {
-		write8bit(memory, SPECIAL_REGISTER_TIMA,
-				memory->spec_reg.tima + 1);
+		timer->spec_reg->tima++;
 	}
 }
 
